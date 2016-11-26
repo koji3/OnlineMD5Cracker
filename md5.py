2a0de0a6e04dd7238cd333fbc30206e0712006c4
Black Viking <yakuza3003@yandex.com>
2016-11-26 13:32:33 +0200

    V.2

--- md5.py
+++ md5.py
@@ -17,16 +17,13 @@ import time
 br = mechanize.Browser()
 br.set_handle_robots(False)
 
-site = "http://md5.my-addr.com/md5_decrypt-md5_cracker_online/md5_decoder_tool.php"
 
 def usage():
 	usage = """[+] Coded By Black Viking
 [+] Online MD5 Cracker Tool
 [+] Version: V.1
-
 Usage:
 	python md5.py -h/--hash <hash>
-
 Example:
 	python md5.py --hash d8578edf8458ce06fbc5bb76a58c5ca4"""
 
@@ -39,21 +36,38 @@ def md5(hash):
 [+] Version: V.1"""
 	print "-"*30
 	print"\n[*] Hash: %s"%(hash)
-	br.open(site)
-	br.select_form(nr=0)
-	br.form["md5"] = hash
-	source = br.submit().read()
-	if 'Hash "'+hash+'" not found in database' in source:
-		print"[-] Hash '%s' not found in database"%(hash)
-		sys.exit()
-	elif 'String "'+hash+'" is not MD5 hash' in source:
-		print"[!] String '%s' is not MD5 hash"%(hash)
-		sys.exit()
-	elif 'Result:' in source:
-		print "[+] Hashed string: %s"%(re.findall("""<span class='middle_title'>Hashed string</span>: (.*?)</div>""", source)[0])
-		sys.exit()
-	else:
-		pass
+
+	def site():
+		br.open("http://md5.my-addr.com/md5_decrypt-md5_cracker_online/md5_decoder_tool.php")
+		br.select_form(nr=0)
+		br.form["md5"] = hash
+		source = br.submit().read()
+		if 'Hash "'+hash+'" not found in database' in source:
+			print"[-] Hash '%s' not found in database"%(hash)
+			pass
+		elif 'String "'+hash+'" is not MD5 hash' in source:
+			print"[!] String '%s' is not MD5 hash"%(hash)
+			sys.exit()
+		elif 'Result:' in source:
+			print "[+] Hashed string: %s"%(re.findall("""<span class='middle_title'>Hashed string</span>: (.*?)</div>""", source)[0])
+			sys.exit()
+		else:
+			pass
+	def site2():
+		br.open("http://md5decryption.com/")
+		br.select_form(nr=0)
+		br.form["hash"] = hash
+		source1 = br.submit("submit").read()
+		if "Decrypted Text:" in source1:
+			print "[+] Hashed string: %s"%(re.findall('Decrypted Text: </b>(.*?))</font>', source1)[0])
+			sys.exit()
+		else:
+			print"[-] Hash '%s' not found in database"%(hash)
+			sys.exit()
+
+	site()
+	site2()
+
 
 
 def main():
